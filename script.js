eb6989f6d2ba1380600bd2c30383f63d7864749b
erika sayuri <erika.s.yamamoto@gmail.com>
2023-05-17 21:44:29 -0300

    [erika sayuri] exercicio 9, 10

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- // ------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------
 //uma paleta contendo quatro cores distintas e suas respectivas classes
  const colorPalette = document.getElementById('color-palette');
 
@@ -15,7 +15,7 @@ function button() {
     addSelect.classList.add('selected');
 }
 button();
- // ------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------
 // botão para gerar cores aleatórias para a paleta de cores e cor preta como a primeira cor da paleta de cores
 function generateRandomColor() {
     const letters = '0123456789ABCDEF'; // Define os caracteres permitidos para representar uma cor hexadecimal
@@ -36,30 +36,40 @@ function generateRandomColor() {
   buttonRandomColor.addEventListener('click', generateRandomColors);
  
  
-  // ------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------
 // função localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
 
- // ------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------
 //função para selecionar uma cor na paleta de cores
-const colors = document.getElementsByClassName('color');
+const colors = document.getElementsByClassName('color'); // Obtém todos os elementos com a classe "color"
 
-function removeSelect() {
+function removeSelect() { // Função para remover a classe "selected" de todos os elementos
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].classList.remove('selected');
   }
 }
 
-function addSelect(event) {
+function addSelect(event) {// Função para adicionar a classe "selected" ao elemento clicado
   event.target.classList.add('selected');
 }
 
-for (let i = 0; i < colors.length; i += 1) {
-  colors[i].addEventListener('click', removeSelect);
-  colors[i].addEventListener('click', addSelect);
+for (let i = 0; i < colors.length; i += 1) { // Adiciona event listeners de clique a cada elemento com a classe "color"
+  colors[i].addEventListener('click', removeSelect); // Quando um elemento é clicado, chama a função removeSelect
+  colors[i].addEventListener('click', addSelect);// Quando um elemento é clicado, chama a função addSelect
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
 //10 - Crie uma função que permita preencher um pixel do quadro com a cor selecionada na paleta de cores
-
+function squareBackground(event) { // Verifica se o elemento clicado possui a classe "pixel"
+    if (event.target.classList.contains('pixel')) { 
+      const getButton = document.querySelector('.selected'); // Obtém o primeiro elemento com a classe "selected"
+      const getBackground = window  // Obtém o valor da propriedade "background-color" do elemento "getButton"
+        .getComputedStyle(getButton) // O resultado é um objeto que representa o estilo CSS aplicado ao elemento.
+        .getPropertyValue('background-color'); // Essa propriedade representa a cor de fundo do elemento.
+      event.target.style.backgroundColor = getBackground; // Aplica a cor de fundo obtida ao elemento clicado
+    }
+  }
+  document.addEventListener('click', squareBackground); // Adiciona um event listener de clique ao documento. Quando ocorre um clique, a função squareBackground é chamada
+//------------------------------------------------------------------------------------------------------------------------------------------
 //11 - Crie um botão que retorne a cor do quadro para a cor inicial
 
 //12 - Crie uma função para salvar e recuperar o seu desenho atual no localStorage
