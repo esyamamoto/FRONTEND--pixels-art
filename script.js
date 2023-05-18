42a1923b6a284a148dbbbcc7abb6bca41c042bdf
erika sayuri <erika.s.yamamoto@gmail.com>
2023-05-17 21:27:44 -0300

    [erika sayuri] exercicios 1, 2, 3, 4, 6, 7, 8, 9

--- script.js
+++ script.js
@@ -1,36 +1,65 @@
-const colorPalette = document.getElementById('color-palette');
+ // ------------------------------------------------------------------------------------------------------------------------------------
+//uma paleta contendo quatro cores distintas e suas respectivas classes
+ const colorPalette = document.getElementById('color-palette');
 
 function button() {
   const arrayColors = ['black', 'red', 'blue', 'green']; // 1 array 4 cores
   for (let index = 0; index < arrayColors.length; index += 1) { // Loop para percorrer o array de cores
     const button = document.createElement('button'); // Cria um elemento <button> para representar cada cor
+    button.id = arrayColors[index];
     button.style.backgroundColor = arrayColors[index];// Define a cor de fundo do botão com base na cor atual do loop
-    button.className = 'color'; //classe de color para os outros botoes
+    button.className = 'color';//classe de color para os outros botoes
     colorPalette.appendChild(button); // Adiciona o botão à paleta de cores
-    }   
+    }
+    const addSelect = document.querySelector('#black');
+    addSelect.classList.add('selected');
 }
 button();
-// 3 - Adicione a cor preta como a primeira cor da paleta de cores
-// 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
-// https://stackoverflow.com/questions/1484506/random-color-generator
+ // ------------------------------------------------------------------------------------------------------------------------------------
+// botão para gerar cores aleatórias para a paleta de cores e cor preta como a primeira cor da paleta de cores
 function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i += 1) {
+    const letters = '0123456789ABCDEF'; // Define os caracteres permitidos para representar uma cor hexadecimal
+    let color = '#'; // Começa com '#' para representar uma cor hexadecimal
+    for (let i = 0; i < 6; i += 1) { // Seleciona aleatoriamente um dígito da string 'letters' para cada um dos seis dígitos da cor
       color += letters[Math.floor(Math.random() * 16)];
     }
-    return color;
+    return color; // Retorna a cor hexadecimal gerada
   }
 
-  function generateRandomColors() {
+  function generateRandomColors() { // Obtém os elementos HTML com a classe 'color'
     const colorBoxes = document.getElementsByClassName('color');
-    for (let index = 1; index < 4; index += 1) {
+    for (let index = 1; index < 4; index += 1) { // Define a cor de fundo de cada elemento com a classe 'color' usando a função generateRandomColor()
       colorBoxes[index].style.backgroundColor = generateRandomColor();
     }
   }
-  const buttonRandomColor = document.getElementById('button-random-color');
+  const buttonRandomColor = document.getElementById('button-random-color'); // Adiciona um evento de clique ao elemento HTML com o id 'button-random-color'
   buttonRandomColor.addEventListener('click', generateRandomColors);
-// 5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
+ 
+ 
+  // ------------------------------------------------------------------------------------------------------------------------------------
+// função localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
 
-//8 - Defina a cor preta como cor inicial da paleta de cores
-//9 - Crie uma função para selecionar uma cor na paleta de cores
+ // ------------------------------------------------------------------------------------------------------------------------------------
+//função para selecionar uma cor na paleta de cores
+const colors = document.getElementsByClassName('color');
+
+function removeSelect() {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
+}
+
+function addSelect(event) {
+  event.target.classList.add('selected');
+}
+
+for (let i = 0; i < colors.length; i += 1) {
+  colors[i].addEventListener('click', removeSelect);
+  colors[i].addEventListener('click', addSelect);
+}
+//------------------------------------------------------------------------------------------------------------------------------------------
+//10 - Crie uma função que permita preencher um pixel do quadro com a cor selecionada na paleta de cores
+
+//11 - Crie um botão que retorne a cor do quadro para a cor inicial
+
+//12 - Crie uma função para salvar e recuperar o seu desenho atual no localStorage
---
+++ scriptBonus.js
@@ -0,0 +1,7 @@
+/*Requisitos Bônus
+13 - Crie um input que permita à pessoa usuária preencher um novo tamanho para o quadro de pixels
+A página deve conter um input para que a pessoa usuária possa definir o tamanho do quadro de pixels
+14 - Crie uma função que limite o tamanho mínimo e máximo do quadro de pixels
+O quadro não pode ser definido com menos de 5 ou mais de 50 pixels
+15 - Crie uma função para manter o tamanho novo do board ao recarregar a página
+O tamanho do board deve ser mantido ao recarregar a página usando localStorage */
